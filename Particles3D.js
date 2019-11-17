7ccc9b50f47d2f08f872087e4f5e47af33192f58
Chris Tralie <chris.tralie@gmail.com>
2019-11-17 18:52:41 -0500

    Making random radii, adding animation based on elapsed time, and coloring spheres with an ambient color so their visibility is improved

--- Particles3D.js
+++ Particles3D.js
@@ -15,7 +15,13 @@ function Particles() {
                         "pos":[0, 20, 0],
                         "color":[1, 1, 1]
                     }
-                ]            
+                ],
+                "materials":{
+                    "redambient":{
+                        "ka":[0.7, 0.0, 0.0],
+                        "kd":[1, 1, 1]
+                    }
+                }          
                 };
     this.floory = 0.0;
     this.floorextent = 1000;
@@ -31,17 +37,19 @@ function Particles() {
 
     this.randomlyInitSpheres = function(N) {
         for (let i = 0; i < N; i++) {
-            pos = [Math.random()*10-5, Math.random()*10, Math.random()*10-5];
+            let pos = [Math.random()*10-5, Math.random()*10, Math.random()*10-5];
+            let radius = 0.5*Math.random();
             let sphere = {
-                "transform":[1, 0, 0, pos[0],
-                             0, 1, 0, pos[1], 
-                             0, 0, 1, pos[2],
+                "transform":[radius, 0, 0, pos[0],
+                             0, radius, 0, pos[1], 
+                             0, 0, radius, pos[2],
                              0, 0, 0, 1],
                 "pos":pos,
+                "radius":radius,
                 "velocity":[Math.random()*0.1, Math.random()*0.1, Math.random()*0.1],
                 "shapes":[
                     {"type":"sphere",
-                    "radius":Math.random()*0.5}
+                    "material":"redambient"}
                 ]
             }
             this.scene.children.push(sphere);
@@ -49,11 +57,17 @@ function Particles() {
         }
     }
 
+    this.time = 0.0;
+    this.lastTime = (new Date()).getTime();
     this.animate = function() {
+        let thisTime = (new Date()).getTime();
+        let dt = (thisTime - this.lastTime)/1000.0; // Change in time in seconds
+        this.time += dt;
+        this.lastTime = thisTime;
         let spheres = this.spheres;
         for (let i = 0; i < spheres.length; i++) {
             for (let k = 0; k < 3; k++) {
-                spheres[i].pos[k] += 0.1*spheres[i].velocity[k];
+                spheres[i].pos[k] += dt*spheres[i].velocity[k];
                 spheres[i].transform[12+k] = spheres[i].pos[k];
             }
             // TODO: Add acceleration and collision effects
